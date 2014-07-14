e7e7bb256577f0871b51c70f2c50a3be22ffaa2b
Jean Lescure <jean@agilityfeat.com>
2014-07-14 17:19:50 -0600

    Pluses.

--- plugins/tess-responder/tess-responder.rb
+++ plugins/tess-responder/tess-responder.rb
@@ -9,12 +9,17 @@ class TessResponder < Tess::Plugin::Base
   def respond_to_message?(message)
     @speaker = message.speaker.titleize.split[0]
     if (message.content =~ /^tess$/i)
-      @@bot.speak(name_response(message.speaker), @@ctype)
+      @@bot.speak(name_response(@speaker)), @@ctype)
       return false
     end
-    if (message.content =~ /^tess\s+(busy|(doing\s+something)|running)/i)
+    if (message.content =~ /^tess\s+.*?(busy|(doing\s+something)|running)/i)
       busy_message = ($tess_busy == 0) ? "I am not doing anything right now." : "I am busy, yes."
       @@bot.speak(busy_message, @@ctype)
+      return false
+    end
+    if (message.content =~ /(tess\s+i|i|\S+)\s+loves{0,1}\s+you/i)
+      @@bot.speak("#{@speaker}, unfortunately I am unable to love.", @@ctype)
+      @@bot.speak('Jean hasn\'t programmed a heart for me yet.', @@ctype)
       return false
     end
     message.content =~ /^tess\s+.*?(help|introduce)/i
