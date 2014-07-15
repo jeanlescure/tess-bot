94b010116219fb0f2749bc8ee80e26336f08d2a1
Giancarlo Palavicini <kasthor@gmail.com>
2014-07-15 08:12:18 -0600

    fix

--- lib/tess/language.rb
+++ lib/tess/language.rb
@@ -1,16 +1,16 @@
 module Tess
   class Language
-    def aye
+    def self.aye
       ['Aye aye Cap\'n','You got it','Sure thing','With pleasure','Right away','You don\'t have to ask me twice',
        'I was coded for this','I was afraid you\'d never ask','Took you long enough'].shuffle.sample
     end
 
-    def dance
+    def self.dance
       ['tango','do the cha cha','salsa','bounce to techno','do the twist','disco',
        'dance the dance','shake my bom bom','feel the mambo'].shuffle.sample
     end
 
-    def enumerate items
+    def self.enumerate items
       case items.length
         when 1 then items.first
         when 2..Float::INFINITY then "#{items[0..-2].join ", "} and #{items[-1]}"
